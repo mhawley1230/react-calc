6d0a254450e9874b2c0c1b9b10fc0b09ddb0b796
mhawley1230@gmail.com <Mike>
2016-12-11 17:05:21 -0500

    created buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      value: 0
+    }
+  }
+
+  update ( e ){
+    console.log(this);
+    this.setState({value: e.target.value})
+  }
+
   render() {
     return (
-      <h1>Hello</h1>
+      <div>
+        <h1>{this.state.value}</h1>
+        <table>
+          <tr>
+            <button onClick={this.update.bind(this)}>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+          </tr>
+          <tr>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+          </tr>
+          <tr>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>x</button>
+          </tr>
+          <tr>
+            <button>C</button>
+            <button>0</button>
+            <button>.</button>
+            <button>/</button>
+            <button>=</button>
+          </tr>
+        </table>
+      </div>
     )
   }
 }
